4590ed5ba802d053b2e22621e133a17243c09e71
Hami Ibriyamov <hamop3108@gmail.com>
2025-05-28 18:13:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,6 @@ def predict():
     return jsonify({'spam': bool(prediction[0])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
